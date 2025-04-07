b835a73ad16f49e695d093a52a71f296fd8a5065
pavandocs4 <pavandocs4@gmail.com>
2025-04-07 13:49:08 +0530

    Initial Commit

---
+++ src/test/java/com/example/java/basics/JavaBasicsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.java.basics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaBasicsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
